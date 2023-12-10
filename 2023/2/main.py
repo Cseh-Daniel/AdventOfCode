5b9c2557ca6cd2856c6b8c726d48ab8ccd341c11
Cseh Daniel <codaryf@gmail.com>
2023-12-10 16:22:55 +0100

    cube game: getting color numbers

--- 2023/2/main.py
+++ 2023/2/main.py
@@ -1,17 +1,38 @@
-f = open('input.txt', 'r')
+f = open('input copy.txt', 'r')
 lines = f.readlines()
 
+colors = ["red", "green", "blue"]
+values = [0,0,0]
 maxAmount = {
     'red': 12,
     'green': 13,
     'blue': 14
 }
 
-for l in lines:
+isValid=False
+
+for gameId,l in enumerate(lines):
+    isValid=False
+    values = [0,0,0]
+   
     l = l[l.index(":") + 2:len(l)]
-    l = l.split(";")
+    l = l.strip("\n").split(";")
 
-    for i,j in l:
+    for i,j in enumerate(l):
         l[i]=j.split(",")
+        for idx, lj in enumerate(l[i]):
+            l[i][idx]=lj.strip(" ")
+            
+            for ci,color in enumerate(colors):
+                currentVal=int(l[i][idx][0])
+                if color in l[i][idx] and currentVal>values[ci]:
+                    values[ci]=currentVal
+    print(values)
+            
 
-    print(l)
+    print(f"{gameId+1}{l}\n{gameId+1} {isValid}\n")
+
+
+
+for i in colors:
+    print(i)
